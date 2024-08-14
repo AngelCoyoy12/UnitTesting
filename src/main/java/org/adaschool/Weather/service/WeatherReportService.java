02568f2e0a257748f0157bfa847de5048dbe47e5
Angel Coyoy <furioso7s12@gmail.com>
2024-08-14 12:00:48 -0600

    Proyecto

--- src/main/java/org/adaschool/Weather/service/WeatherReportService.java
+++ src/main/java/org/adaschool/Weather/service/WeatherReportService.java
@@ -8,14 +8,27 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class WeatherReportService {
 
-    private static final String API_KEY = "your-api-key";
+    private static final String API_KEY = "aeb07a35fc7d2fc788ab33e2211664a9"; // Agrego mi API key
     private static final String API_URL = "https://api.openweathermap.org/data/2.5/weather";
 
+    private final RestTemplate restTemplate;
+    public WeatherReportService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+
+    }
+
     public WeatherReport getWeatherReport(double latitude, double longitude) {
-        RestTemplate restTemplate = new RestTemplate();
+
         String url = API_URL + "?lat=" + latitude + "&lon=" + longitude + "&appid=" + API_KEY;
+
         WeatherApiResponse response = restTemplate.getForObject(url, WeatherApiResponse.class);
 
+        // Verificar si la respuesta o el objeto 'Main' son nulos
+        if (response == null || response.getMain() == null) {
+            return null;
+        }
+
+        // Crear el reporte del clima
         WeatherReport report = new WeatherReport();
         WeatherApiResponse.Main main = response.getMain();
         report.setTemperature(main.getTemperature());
